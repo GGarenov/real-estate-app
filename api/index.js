1c20de717dbcac42146469ba620627480c741432
Georgiy Garenov <g_garenov@outlook.com>
2023-11-06 15:27:21 +0200

    Successfully connected and configured MongoDB and Mongoose env was installed in order to hide our MongoDB username and password.

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { error } from "console";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log(`Uspeshno se vurzahme za MongoDB!`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
