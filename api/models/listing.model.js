65cfd37ddba7a5e0dd1649d5083d6406c4bed339
Georgiy Garenov <g_garenov@outlook.com>
2024-02-07 21:57:57 +0200

    A telephone number was added in the listing model

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -14,6 +14,10 @@ const listingSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    tel: {
+      type: String,
+      required: true,
+    },
     regularPrice: {
       type: Number,
       required: true,
