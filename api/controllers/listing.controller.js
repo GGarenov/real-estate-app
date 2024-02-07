9b69c7aa439d9ca5a12dc1f0fc84fc0f3ba365ed
Georgiy Garenov <g_garenov@outlook.com>
2024-02-07 22:02:46 +0200

    console log removed

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -3,7 +3,6 @@ import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
-    console.log(req.body);
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
   } catch (error) {
